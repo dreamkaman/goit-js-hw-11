c19402131ebf3bb8ecc71974227804a1c1fad76b
Vlad <64969216+dreamkaman@users.noreply.github.com>
2021-11-25 00:24:33 +0200

    next

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import './sass/main.scss';
 import axios from "axios";
 import Notiflix from 'notiflix';
 
+const NotiflixSettings = { fontSize: '20px', width: '500px' };
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '24499168-c2c7d94c072f729a438874bbc';
 
-const getImages = query => {
-    return axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`).then(res => {
+const getImages = (query, page) => {
+
+    return axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+        .then(res => {
         
-        const {hits} = res.data;
+            const { total, hits } = res.data;
+            
+            //const [{ hit: { comments, likes}}] = hits;
 
         console.log(res.data);
-        if (hits.length === 0) { Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')};
+
+        if (total === 0) {
+
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.', NotiflixSettings)
+        }
+        else {
+            const markup = hits.map(el => {
+                
+            })
+        }
     }
     )
 };
@@ -19,6 +34,8 @@ const getImages = query => {
 const inputEl = document.querySelector('input');
 
 const formEl = document.querySelector('form');
+
+const galleryEl = document.querySelector('.gallery');
 
 // console.log(inputEl, formEl);
 
@@ -28,7 +45,7 @@ const onFormSubmit = event => {
 
     const query = inputEl.value;
 
-   getImages(query); 
+   getImages(query, 1); //============================= by default page = 1;
 
 };
 
